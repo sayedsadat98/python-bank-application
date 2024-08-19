26b9f547604f5ad8a68e955680bd003131c74a2f
Sayed Sadat <sayedsadat98@gmail.com>
2024-08-19 13:16:53 -0400

    Refactor: Docstrings added

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 class BankAccount():
 
     def __init__(self, initial_balance: float):
+        """
+            Initializes a new BankAccount instance with an initial balance.
+            Args:
+                initial_balance (float): The initial balance of the account.
+            Raises:
+                ValueError: If the initial balance is negative.
+        """
         if initial_balance < 0:
             raise ValueError("Initial Balance cannot be negative!")
 
         self.balance = initial_balance
 
     def deposit(self, amount: float):
+        """
+            Deposits a specified amount into the bank account.
+            Args:
+                amount (float): Non-negative amount to be deposited.
+            Raises:
+                ValueError: If the deposit amount is negative.
+        """
         if amount < 0:
             raise ValueError("You must deposit a valid amount")
 
@@ -14,6 +28,13 @@ class BankAccount():
         print("Deposited Successfully!\nNew Balance: $", self.balance)
 
     def withdraw(self, amount: float):
+        """
+            Withdraws a specified amount from the bank account.
+            Args:
+                amount (float): The amount to withdraw.
+            Raises:
+                ValueError: If the withdrawal amount > current balance.
+        """
         if amount > self.balance:
             raise ValueError("Insufficient Fund!")
 
@@ -23,6 +44,7 @@ class BankAccount():
         return self.balance
 
 
+# Demo
 if __name__ == '__main__':
     myBank = BankAccount(1000)
     myBank.deposit(1200)
